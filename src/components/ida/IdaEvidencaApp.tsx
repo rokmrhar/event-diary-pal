7e3d9e0e1e65c713becd33e911703377cd00d9a2
Rok Mrhar <rokmrhar9@gmail.com>
2026-04-28 14:09:20 +0200

    Update IdaEvidencaApp.tsx
    podaljšana leta

--- src/components/ida/IdaEvidencaApp.tsx
+++ src/components/ida/IdaEvidencaApp.tsx
@@ -70,7 +70,7 @@ interface IdaEvidencaAppProps {
 }
 
 const currentYear = new Date().getFullYear();
-const YEARS = Array.from({ length: 80 }, (_, i) => currentYear - i);
+const YEARS = Array.from({ length: 100 }, (_, i) => currentYear - i);
 const MONTHS = [
   "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12",
 ];
